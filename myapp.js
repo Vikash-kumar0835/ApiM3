e06c7aa9ba386738fd5159c0f55224a7d8397575
Vikash-kumar0835 <vikash.kumar.phe16@itbhu.ac.in>
2021-05-15 14:36:23 +0530

    justcommit

--- myapp.js
+++ myapp.js
@@ -21,6 +21,23 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+
+app.get('/',function(req,res){
+  res.json("vikashkumar")
+});
+
+
+
+
+
+
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
+
+
+/*
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -29,7 +46,4 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
-
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+*/
